49e26f0031139690c6629b9dd8f823e26e5bb4df
giatung2002 <70301943+giatung2002@users.noreply.github.com>
2024-05-17 23:22:23 -0700

    make changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
 import Contact from './sections/Contact/Contact';
 import Footer from './sections/Footer/Footer';
-import Hero from './sections/Hero/Hero';
+import Hero from './sections/Hero/tungis';
 import Projects from './sections/Projects/Projects';
 import Skills from './sections/Skills/Skills';
 
